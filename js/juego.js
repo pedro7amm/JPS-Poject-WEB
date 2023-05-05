87999be9eb4be23f4cb859499ad6be04c68019f4
Pedro <pedrolendez7@gmail.com>
2023-05-05 10:04:55 -0600

    Uploading new new html and new js

---
+++ js/juego.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function runGame(numUser, serUser){
+    document.getElementById("show").innerHTML="Su número de billete es: "+numUser;
+    document.getElementById("show").innerHTML+="<br>";
+    document.getElementById("show").innerHTML+="Su número de serie de billete es: "+serUser;
+
+}
